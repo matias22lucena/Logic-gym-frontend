b37117f8300ddb26f2c26987a90e6078a66e9eb2
Mariano Juarez <nanito.mj@gmail.com>
2026-06-30 08:55:02 -0300

    se agrego la estructura del navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,77 @@
+import { Navbar, Container, Nav, Form, Button } from "react-bootstrap";
+import { Link, NavLink, useNavigate } from "react-router";
+import "./Navbar.css";
+
+const Navbar = ({ usuarioLogueado, setUsuarioLogueado }) => {
+  const navegacion = useNavigate();
+  const logout = () => {
+    setUsuarioLogueado(false);
+  };
+
+  return (
+    <Navbar expand="lg" className="custom-navbar">
+      <Container fluid className="px-4 px-lg-5">
+
+        <Navbar.Brand as={Link} to={"/"} className="logo-text">
+        Logic Gym
+        </Navbar.Brand>
+
+        <Navbar.Toggle aria-controls="navbar-nav"/>
+
+        <Navbar.Collapse className="navbar-nav">
+
+        <Nav className="mx-auto gap-5">
+
+          {usuarioLogueado && usuarioLogueado.role === "admin" && (
+              
+                <NavLink as={NavLink} to={"/administrador"} className="custom-link">
+                  Administrador
+                </NavLink>
+                
+            )}
+
+          <Nav.Link as={NavLink} to={"/"} className="custom-link">
+          Inicio
+          </Nav.Link>
+
+          <Nav.Link as={NavLink} to={"/planes"} className="custom-link">
+          Planes
+          </Nav.Link>
+
+
+          <Nav.Link as={NavLink} to={"/contacto"} className="custom-link">
+          Contacto
+          </Nav.Link>
+
+          <Nav.Link as={NavLink} to={"/nosotros"} className="custom-link">
+          Nosotros
+          </Nav.Link>
+
+          {!usuarioLogueado && (
+            <>
+          <Nav.Link as={NavLink} to={"/login"} className="custom-link">
+          Login
+          </Nav.Link>
+
+          <Nav.Link as={NavLink} to={"/Registrate"} className="custom-link">
+          Registrate
+          </Nav.Link>
+          </>
+          )}
+
+          {usuarioLogueado && (
+              <Button variant="dark" onClick={logout}>
+                  Logout
+                </Button>
+              
+            )}
+
+        </Nav>
+        </Navbar.Collapse>
+
+      </Container>
+    </Navbar>
+  )
+}
+
+export default Navbar
