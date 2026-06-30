7d9abf149e5c3ea18ae789f7c511f8e524af4d7b
Milagros del Carmen Acuña <milagrosdelc49@gmail.com>
2026-06-30 19:48:35 -0300

    agregue los imports del home en la rama feature/inicio

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState, useContext } from 'react';
+import { Container, Row, Col, Card, Button, Carousel } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom'; 
+
+
+
+
+
+import './App.css';
+
+import publicidad0 from '../assets/publicidad0.jpg';
+import publicidad1 from '../assets/publicidad1.jpg';
+import publicidad2 from '../assets/publicidad2.webp';
+import publicidad3 from '../assets/publicidad3.jpg';
+import publicidad4 from '../assets/publicidad4.png';
+import publicidad5 from '../assets/publicidad5.jpg';
+
+import imgCuerda from '../assets/cuerda6.jpg';
+import imgRemera from '../assets/remera5.jpg';
+import imgShaker from '../assets/Shaker4.jpg';
+import imgGuantes from '../assets/Guantes3.jpg';
+import imgCreatina from '../assets/creatina.jpg';
+import imgProteina from '../assets/proteina1.jpg';
+import heroVideo from '../assets/videoLogicGym.mp4';
+
+import asesoramiento from '../assets/asesoramientonutricional.webp';
+import box from '../assets/clasesdeBoxCrossfit.jpg';
+import clasesg from '../assets/clasesgrupales.jpg';
+import salam from '../assets/SaladeMusculación.jpg';
+import evaluacionf from '../assets/evaluacionfisica.jpg';
+import yoga from '../assets/yoga.jpg';
