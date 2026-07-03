8c41dd715ec3d5f82e50a28f54e11b8b4c48e78b
matias22lucena <matias22lucena@gmail.com>
2026-07-03 01:29:10 -0300

    Agregamos la rutas de administrador

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,52 @@
-import { BrowserRouter, Route, Routes } from "react-router"
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import BarraNavegacion from "./components/navbar/BarraNavegacion";
-import ProtectorAdmin from "./components/routes/ProtectorAdmin"
 import Registro from "./pages/RegistroPages";
 import Login from "./pages/IniciarSesionPage";
-import Footer from "./components/footer/Footer"
-import Home from "./pages/HomePage";
-import { CartProvider } from './components/carrito/carrito.jsx';
+import Footer from "./components/footer/Footer";
+import { CartProvider } from "./components/carrito/carrito.jsx";
 import SobreNosotros from "./pages/SobreNosotros.jsx";
 import Contacto from "./pages/Contacto.jsx";
-
-
 import HomePage from "./pages/HomePage";
+
 const App = () => {
   const sesionUsuario =
     JSON.parse(sessionStorage.getItem("usuarioKey")) || false;
+
   const [usuarioLogueado, setUsuarioLogueado] = useState(sesionUsuario);
-  const [productos, setProductos] = useState([]);
 
   useEffect(() => {
     sessionStorage.setItem("usuarioKey", JSON.stringify(usuarioLogueado));
   }, [usuarioLogueado]);
 
   return (
-    <>
     <BrowserRouter>
-    <CartProvider>
-    <BarraNavegacion></BarraNavegacion>
+      <CartProvider>
+        <BarraNavegacion
+          usuarioLogueado={usuarioLogueado}
+          setUsuarioLogueado={setUsuarioLogueado}
+        />
 
-   <main>
-    <Routes>
-      <Route path="/" element={<HomePage/>}/>
-      <Route path="/contacto" element={<Contacto/>}/>
-  <Route path="/sobrenosotros" element={<SobreNosotros />} />
-            
-      
-{/*       <Route path="/" element={<Home/>}/>
-      <Route path="/detalle" element={<DetalleDeProducto/>}/>
-      <Route path="/login" element={<Login setUsuarioLogueado={setUsuarioLogueado}/>}/>
-     
-      <Route path="/contacto" element={<Contacto/>}/>
-      <Route path="/administrador" element={
-        <ProtectorAdmin usuarioLogueado={usuarioLogueado}></ProtectorAdmin>}>
-      <Route index element={<Administrador productos={productos} setProductos={setProductos}></Administrador>}/>
-      <Route path="crear" element={<FormularioProducto titulo="Crear Producto"></FormularioProducto>}/>
-      <Route path="editar/:id" element={<FormularioProducto titulo="Editar Producto"></FormularioProducto>}/>
-      </Route>
-      <Route path="*" element={<Error404></Error404>}/> */}
+        <main>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/contacto" element={<Contacto />} />
+            <Route path="/sobrenosotros" element={<SobreNosotros />} />
 
-      <Route path="/login" element={<Login setUsuarioLogueado={setUsuarioLogueado}/>}/>
-       <Route path="/registro" element={<Registro/>}/>
-    </Routes>
-   </main>
-<Footer></Footer> 
-</CartProvider>
-   </BrowserRouter>
-    </>
+            <Route
+              path="/login"
+              element={<Login setUsuarioLogueado={setUsuarioLogueado} />}
+            />
+
+            <Route path="/registro" element={<Registro />} />
+          </Routes>
+        </main>
+
+        <Footer />
+      </CartProvider>
+    </BrowserRouter>
   );
 };
 
